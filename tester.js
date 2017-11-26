b3f50e9f48d92e40d98d8d54b7703bf40871d619
Craig Cahillane <craig-cahillane.ligo.org>
2017-11-26 01:12:12 -0600

    Messing around with javascript and mathjax

--- tester.js
+++ tester.js
@@ -1,9 +1,11 @@
-TESTER = document.getElementById('tester');
+<script>
+    TESTER = document.getElementById('tester');
 
-Plotly.plot( TESTER, [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16] }], { 
-    margin: { t: 0 } } );
+    Plotly.plot( TESTER, [{
+        x: [1, 2, 3, 4, 5],
+        y: [1, 2, 4, 8, 16] }], { 
+        margin: { t: 0 } } );
 
-/* Current Plotly.js version */
-console.log( Plotly.BUILD );
+    /* Current Plotly.js version */
+    console.log( Plotly.BUILD );
+</script>
